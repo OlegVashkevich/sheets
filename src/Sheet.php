9aeb82182c5f70cd9a93f9504cc4f685975f70de
Oleg Vashkevich <ugluk@tut.by>
2025-03-08 11:22:09 +0300

    + clear, update, get

--- src/Sheet.php
+++ src/Sheet.php
@@ -4,16 +4,21 @@ namespace OlegV;
 
 use Exception;
 
-class Sheet {
+class Sheet
+{
     private Credentials $credentials;
     private string $access_token = '';
     private string $sheet;
+
     /**
+     * @param  string  $spreadsheet_id
+     * @param  string  $credentials_path
      * @throws Exception
      */
-    public function __construct(private readonly string $spreadsheet_id, string $credentials_path) {
+    public function __construct(private readonly string $spreadsheet_id, string $credentials_path)
+    {
         $credentials = file_get_contents($credentials_path);
-        if(is_string($credentials)) {
+        if (is_string($credentials)) {
             $data = json_decode($credentials, true);
             $this->credentials = new Credentials($data);
             $this->auth();
@@ -23,25 +28,9 @@ class Sheet {
     }
 
     /**
-     * @throws Exception
+     * @param  string  $sheet
+     * @return $this
      */
-    public function auth(): void
-    {
-        $token = $this->getAuthToken();
-        if($token!=''){
-            $data = [
-                'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-                'assertion' => $token,
-            ];
-            $response = $this->request($this->credentials->token_uri, http_build_query($data));
-            $array = json_decode($response, true);
-            if(is_array($array) && isset($array['access_token']) && is_string($array['access_token'])){
-                $this->access_token = $array['access_token'];
-            }
-        } else {
-            throw new Exception('Problem with creating token.');
-        }
-    }
     public function setSheet(string $sheet): static
     {
         $this->sheet = $sheet;
@@ -51,64 +40,143 @@ class Sheet {
     /**
      * @param  array<int, array<int,(int|float|string)>>  $values
      * @param  string  $range
-     * @return void
+     * @return string
      * @throws Exception
      */
-    public function append(array $values, string $range = ''):void
+    public function append(array $values, string $range = ''): string
     {
-        $authorization = "Authorization: Bearer ".$this->access_token;
         $range = $range ? "!$range" : '';
-        $body =  json_encode([
-            'values' => $values
+        $body = json_encode([
+            'values' => $values,
         ]);
-        $uri = 'https://sheets.googleapis.com/v4/spreadsheets/'.$this->spreadsheet_id.'/values/'.$this->sheet.$range.':append?valueInputOption=RAW&alt=json';
-        if(is_string($body)) {
-            $response = $this->request($uri, $body, $authorization);
-            $array = json_decode($response, true);
-            var_dump($array);
+        $uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
+            .$this->spreadsheet_id.'/values/'.$this->sheet.$range.':append?valueInputOption=RAW&alt=json';
+        if (is_string($body)) {
+            $response = $this->request($uri, $body);
+            return $this->validateResponse($response);
         }
-
+        return '';
     }
 
-    //TODO get method
-    //TODO get update
+    /**
+     * @param  string  $range
+     * @return string
+     * @throws Exception
+     */
+    public function clear(string $range = ''): string
+    {
+        $range = $range ? "!$range" : '';
+        $uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
+            .$this->spreadsheet_id.'/values/'.$this->sheet.$range.':clear?alt=json';
 
+        $response = $this->request($uri, '');
+        return $this->validateResponse($response);
+    }
+
+    /**
+     * @param  array<int, array<int,(int|float|string)>>  $values
+     * @param  string  $range
+     * @return string
+     * @throws Exception
+     */
+    public function update(array $values, string $range = ''): string
+    {
+        $range = $range ? "!$range" : '';
+        $body = json_encode([
+            'values' => $values,
+        ]);
+        $uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
+            .$this->spreadsheet_id.'/values/'.$this->sheet.$range.'?valueInputOption=RAW&alt=json';
+        if (is_string($body)) {
+            $response = $this->request($uri, $body, 'PUT');
+            return $this->validateResponse($response);
+        }
+        return '';
+    }
+
+    /**
+     * @param  string  $range
+     * @return string
+     * @throws Exception
+     */
+    public function get(string $range = ''): string
+    {
+        $range = $range ? "!$range" : '';
+        $uri = 'https://sheets.googleapis.com/v4/spreadsheets/'
+            .$this->spreadsheet_id.'/values/'.$this->sheet.$range.'?alt=json';
+
+        $response = $this->request($uri, '', 'GET');
+        return $this->validateResponse($response);
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function auth(): void
+    {
+        $token = $this->getAuthToken();
+        if ($token != '') {
+            $data = [
+                'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'assertion' => $token,
+            ];
+            $response = $this->request($this->credentials->token_uri, http_build_query($data));
+            $array = json_decode($response, true);
+            if (is_array($array) && isset($array['access_token']) && is_string($array['access_token'])) {
+                $this->access_token = $array['access_token'];
+            }
+        } else {
+            throw new Exception('Problem with creating token.');
+        }
+    }
+
+    /**
+     * @return string
+     */
     private function getAuthToken(): string
     {
         $header = [
             "alg" => "RS256",
             "typ" => "JWT",
-            "kid" => $this->credentials->private_key_id
+            "kid" => $this->credentials->private_key_id,
+        ];
+
+        $scope = [
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.file",
+            "https://www.googleapis.com/auth/drive.readonly",
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/spreadsheets.readonly",
         ];
 
         $payload = [
             "iss" => $this->credentials->client_email,
             "sub" => $this->credentials->client_email,
-            "scope" => "https://www.googleapis.com/auth/spreadsheets https://www.googleapis.com/auth/spreadsheets.readonly",
+            "scope" => implode(" ", $scope),
             "aud" => $this->credentials->token_uri,
             "iat" => time(),
-            "exp" => time()+3600
+            "exp" => time() + 3600,
         ];
 
         $isError = false;
         $signature = '';
 
         $header = json_encode($header, JSON_UNESCAPED_SLASHES);
-        if(is_string($header)) {
+        if (is_string($header)) {
             $header = rtrim(strtr(base64_encode($header), '+/', '-_'), '=');
         } else {
             $isError = true;
         }
         $payload = json_encode($payload, JSON_UNESCAPED_SLASHES);
-        if(is_string($payload)) {
+        if (is_string($payload)) {
             $payload = rtrim(strtr(base64_encode($payload), '+/', '-_'), '=');
         } else {
             $isError = true;
         }
         $key = openssl_pkey_get_private($this->credentials->private_key);
-        if(!is_bool($key)) {
+        if (!is_bool($key)) {
             $isOK = openssl_sign($header.'.'.$payload, $signature, $key, OPENSSL_ALGO_SHA256);
-            if($isOK && is_string($signature)) {
+            if ($isOK && is_string($signature)) {
                 $signature = base64_encode($signature);
                 $signature = rtrim(strtr($signature, '+/', '-_'), '=');
             } else {
@@ -118,37 +186,59 @@ class Sheet {
             $isError = true;
         }
 
-        if(!$isError && is_string($header) && is_string($payload) && is_string($signature)) {
-            return  $header . '.' . $payload . '.' . $signature;
+        if (!$isError && is_string($header) && is_string($payload) && is_string($signature)) {
+            return $header.'.'.$payload.'.'.$signature;
         }
 
         return '';
     }
+
     /**
      * @param  string  $uri
      * @param  string  $post
-     * @param  string  $authorization
+     * @param  string  $method
      * @return string
      * @throws Exception
      */
-    private function request(string $uri, string $post, string $authorization = ''):string
+    private function request(string $uri, string $post, string $method = 'POST'): string
     {
         $ch = curl_init($uri);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
+        if ($method == 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+        } elseif ($method) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+        }
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        if($authorization != '') {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json' , $authorization ));
+        if ($this->access_token != '') {
+            $authorization = "Authorization: Bearer ".$this->access_token;
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json', $authorization]);
         }
         $response = curl_exec($ch);
-        if($response === false)
-        {
+        if ($response === false) {
             throw new Exception(curl_error($ch));
-        } elseif(is_string($response)) {
+        } elseif (is_string($response)) {
             return $response;
         }
         return '';
     }
+
+    /**
+     * @param  string  $response
+     * @return string
+     * @throws Exception
+     */
+    private function validateResponse(string $response): string
+    {
+        $array = json_decode($response, true);
+        if (is_array($array) && isset($array['error']) && is_array($array['error'])) {
+            if (isset($array['error']['message']) && is_string($array['error']['message'])) {
+                throw new Exception($array['error']['message']);
+            }
+        }
+        return $response;
+    }
+
 }
--- tests/SheetTest.php
+++ tests/SheetTest.php
@@ -12,10 +12,47 @@ class SheetTest extends TestCase
     /**
      * @throws Exception
      */
-    public function testAuth(): void
+    public function testAppend(): void
     {
-        $sheet = new Sheet("1APvvpvUseIDJAvomQHeDvjHhbAjilSZvTR_P1UcwiDs",__DIR__.'/../config/sheet.json');
-        $sheet->setSheet('Sheet9')->append([[1,2,"Text","Текст","many words many words many words",6,7,8,9]], 'R1C1');
+        $sheet = new Sheet("1cPmurA1B6TzUCvchc_wG3YnI0DwN0_9ZS5nsnZO8PRw", __DIR__.'/../config/sheet.json');
+        $response = $sheet->setSheet('Sheet1')->append(
+            [[1, 2, "Text", "Текст", "many words many words many words", 6, 7, 8, 9]],
+            'R1C1',
+        );
+        $this->expectNotToPerformAssertions();
+
+        //delete
+        $data = json_decode($response, true);
+        if (is_array($data) && isset($data['updates']) && is_array($data['updates'])) {
+            if (isset($data['updates']['updatedRange']) && is_string($data['updates']['updatedRange'])) {
+                $range = explode('!', $data['updates']['updatedRange']);
+                $response = $sheet->clear($range[1]);
+                print_r($response);
+            }
+        }
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function testGetAndUpdate(): void
+    {
+        $sheet = new Sheet("1cPmurA1B6TzUCvchc_wG3YnI0DwN0_9ZS5nsnZO8PRw", __DIR__.'/../config/sheet.json');
+        $sheet->setSheet('Sheet1');
+        $response = $sheet->get(
+            'A6:C6',
+        );
+
+        $response = $sheet->update(
+            [["Text", "Текст", "many words many words many words"]],
+            'A6:C6',
+        );
+
+        //return
+        $sheet->update(
+            [[1, 2, 3]],
+            'A6:C6',
+        );
         $this->expectNotToPerformAssertions();
     }
 }
